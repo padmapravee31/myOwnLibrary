3c63d685499d1dc7a2769a41f662d360de234543
padmapravee31 <70197094+padmapravee31@users.noreply.github.com>
2020-09-07 13:55:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var fixedRect,movingRect,gameObject1,gameObject2;
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 200, 100, 50);
+  movingRect = createSprite(500,300,50,100);
+  gameObject1 = createSprite(200, 200, 50, 50);
+  gameObject2 = createSprite(100, 200, 50, 50);
+  gameObject1.velocityX = -1;
+  gameObject2.velocityX = 1;
+}
+
+function draw() {
+  background(255,255,255); 
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+  if(isTouching(fixedRect,movingRect)){
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "red";
+  }
+  else{
+    fixedRect.shapeColor = "green";
+    movingRect.shapeColor = "green";
+  }
+
+  bounceOff(gameObject1,gameObject2);
+  
+  drawSprites();
+}
